1ed18259c47df6fc6abef7d31f3a457ac2694fce
Espezzialy Souza <espezzialy@outlook.com>
2024-02-17 19:47:05 -0300

    feat: start designing posts

--- src/app/home/PostCard.tsx
+++ src/app/home/PostCard.tsx
@@ -2,12 +2,18 @@ import type { Post } from "@prisma/client";
 
 interface PostCardProps {
   post: Post;
+  className?: string;
 }
 
-export default function PostCard({ post }: PostCardProps) {
+export default function PostCard({ post, className }: PostCardProps) {
   return (
-    <div>
-      <h3>{post.title}</h3>
+    <div
+      className={`
+      ${className}
+      p-4 rounded border-2
+      shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]`}
+    >
+      <h3 className="text-2xl sm:text-3xl mb-3">{post.title}</h3>
       <p>{post.content}</p>
     </div>
   );
--- src/app/home/Posts.tsx
+++ src/app/home/Posts.tsx
@@ -4,17 +4,16 @@ import PostCard from "./PostCard";
 const prisma = new PrismaClient();
 
 export default async function Posts() {
-  const users = await prisma.user.findMany();
-  console.log(users);
   const posts = await prisma.post.findMany();
-  console.log(posts);
+
+  const bgClasses = ["bg-pink-500", "bg-purple-500", "bg-blue-500"];
 
   return (
     <div>
       <h2 className="text-4xl text-center mt-6">Trending</h2>
-      <div className="grid grid-cols-3 gap-4">
-        {posts.map((post) => (
-          <PostCard key={post.id} post={post} />
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
+        {posts.map((post, index) => (
+          <PostCard key={post.id} post={post} className={bgClasses[index]} />
         ))}
       </div>
     </div>
