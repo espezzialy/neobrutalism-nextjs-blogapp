749ed63994ddbc71b707d2936bd6cf685187a8cd
Espezzialy Souza <espezzialy@outlook.com>
2024-02-16 21:46:02 -0300

    feat: starting making header and add all public images in public folder

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="border-b flex justify-between">
+            <div className="px-2 py-4 text-4xl">LOGO</div>
+            <button className="text-white bg-black px-4 py-4 ">Sign In</button>
+        </div>
+    )
+}
